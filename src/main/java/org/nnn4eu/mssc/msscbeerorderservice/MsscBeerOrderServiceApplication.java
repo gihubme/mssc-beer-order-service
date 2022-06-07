b6adbb13192442b07389d6cf5c8e2776db316d54
gihubme <n.karakin@qmul.ac.uk>
2022-06-07 13:29:04 +0200

    initial

---
+++ src/main/java/org/nnn4eu/mssc/msscbeerorderservice/MsscBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.mssc.msscbeerorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nnn4eu/mssc/msscbeerorderservice/MsscBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nnn4eu.mssc.msscbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
