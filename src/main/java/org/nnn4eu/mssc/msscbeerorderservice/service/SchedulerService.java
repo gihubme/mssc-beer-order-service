366b6e484dcee97b7d167e2a949de11b9d9a7f8f
gihubme <n.karakin@qmul.ac.uk>
2022-06-12 21:02:04 +0200

    optimise imports

--- src/main/java/org/nnn4eu/mssc/msscbeerorderservice/service/SchedulerService.java
+++ src/main/java/org/nnn4eu/mssc/msscbeerorderservice/service/SchedulerService.java
@@ -2,7 +2,6 @@ package org.nnn4eu.mssc.msscbeerorderservice.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
